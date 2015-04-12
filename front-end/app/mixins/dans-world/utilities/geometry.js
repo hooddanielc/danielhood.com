d849f12fa26a45b73c8327add7745ff96eb449f2
hooddanielc <hood.danielc@gmail.com>
2015-04-12 02:58:15 -0700

    adds placeholder geometry and shader classes

---
+++ front-end/app/mixins/dans-world/utilities/geometry.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+/**
+ * An object that represents geometry.
+ *
+ * @class Geometry
+ */
+export default Ember.Object.extend({
+  intialize: function () {
+    console.log('TODO');
+  }.on('init')
+});
---
+++ front-end/app/mixins/dans-world/utilities/shader.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+/**
+ * A utility class for compiling a shader
+ * 
+ * @class Shader
+ */
+export default Ember.Object.extend({
+  initialize: function () {
+    console.log('TODO');
+  }.on('init')
+});
+

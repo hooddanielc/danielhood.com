36b0e946db72c49287e3bbb5156076978e2b8dd6
hooddanielc <hood.danielc@gmail.com>
2015-05-04 20:51:49 -0700

    finishes http readstream implementation

--- front-end/app/mixins/dans-world/utilities/http-read-stream.js
+++ front-end/app/mixins/dans-world/utilities/http-read-stream.js
@@ -8,12 +8,56 @@ import Ember from 'ember';
  *
  * @class HttpReadStream
  */
-export default Ember.Object.extend({
+export default Ember.Object.extend(Ember.Evented, {
   url: null,
 
   read: function () {
-    return new Ember.RSVP.Promise(function () {
-      // TODO
+    var self = this;
+
+    return new Ember.RSVP.Promise(function (resolve) {
+      Ember.assert('url is a truthy value', self.get('url'));
+      var req = new window.XMLHttpRequest();
+      self.trigger('XMLHttpRequestCreated', req);
+      req.open("GET", self.get('url'), true);
+      var cursor = 0;
+
+      req.onloadstart = function (progressEvent) {
+        self.trigger('loadstart', progressEvent);
+      };
+
+      req.onprogress = function (progressEvent) {
+        var data = progressEvent.currentTarget.responseText.substring(
+          cursor,
+          progressEvent.position
+        );
+
+        cursor = progressEvent.position;
+        self.trigger('progress', progressEvent);
+        self.trigger('data', data);
+      };
+
+      req.onabort = function (progressEvent) {
+        self.trigger('abort', progressEvent);
+      };
+
+      req.onerror = function (progressEvent) {
+        self.trigger('error', progressEvent);
+      };
+
+      req.onload = function (progressEvent) {
+        self.trigger('load', progressEvent);
+      };
+
+      req.ontimeout = function (progressEvent) {
+        self.trigger('timeout', progressEvent);
+      };
+
+      req.onloadend = function (progressEvent) {
+        self.trigger('loadend', progressEvent);
+        resolve();
+      };
+
+      req.send();
     });
   }
 });
--- front-end/tests/unit/dans-world/utilities/http-read-stream-test.js
+++ front-end/tests/unit/dans-world/utilities/http-read-stream-test.js
@@ -1,5 +1,5 @@
-import NeedsCanvas from '../../../helpers/needs-canvas';
 import { test, module } from 'qunit';
+import { expect } from 'chai';
 import Ember from 'ember';
 import HttpReadStream from 'phreaker-eyes/mixins/dans-world/utilities/http-read-stream';
 
@@ -7,7 +7,34 @@ test('exists', function (assert) {
   assert.ok(HttpReadStream);
 });
 
-test('it loads json', function (assert) {
-  // TODO - create a world instance
-  assert.ok(HttpReadStream);
+test('it creates an XMLHttpRequest object', function (assert) {
+  var done = assert.async();
+
+  HttpReadStream.create({
+    url: '/fixtures/wave-front/joel-cube/joel-cube-warped.wavefront'
+  }).on("XMLHttpRequestCreated", function (req) {
+    expect(req instanceof window.XMLHttpRequest).to.eql(true);
+    assert.ok(req);
+  }).read().then(function () {
+    done();
+  }).catch(function (err) {
+    throw err;
+  });
+});
+
+test('it loads text through data event', function (assert) {
+  var done = assert.async();
+  var text = '';
+
+  HttpReadStream.create({
+    url: '/fixtures/wave-front/joel-cube/joel-cube-warped.wavefront'
+  }).on('data', function (data) {
+    assert.ok(data);
+    text += data;
+  }).read().then(function () {
+    expect(text).to.be.a('string');
+    done();
+  }).catch(function (err) {
+    throw err;
+  });
 });

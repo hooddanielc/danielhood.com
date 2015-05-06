d5b52c4141fbdb0f26b5d344bd502c4965cbedd1
hooddanielc <hood.danielc@gmail.com>
2015-05-05 21:27:21 -0700

    adding some todos for WaveFrontParser

--- front-end/app/mixins/dans-world/utilities/wave-front-parser.js
+++ front-end/app/mixins/dans-world/utilities/wave-front-parser.js
@@ -10,6 +10,30 @@ export default Ember.Object.extend({
   objFile: null,
   loadingPromise: null,
   _httpLoaderPromises: null,
+  _rawQueue: null,
+  _lineQueue: null,
+  _state: null,
+  _states: null,
+  _popMaxRaw: 4000,
+  _popMaxLines: 500,
+
+  /**
+   * shifts an amount characters
+   * off of the raw queue,
+   * and turns them into lines
+   */
+  idleParseRawTick: function () {
+    // TODO
+  },
+
+  /**
+   * Shifts an amount of lines
+   * off of the line queue
+   * and invokes parse events
+   */
+  idleParseLine: function () {
+    // TODO
+  },
 
   load: function () {
     var p = new Ember.RSVP.Promise(function () {

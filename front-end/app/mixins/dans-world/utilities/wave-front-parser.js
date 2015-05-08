8f0ccfccd0718d92c95121a0e1a2d3695da9ec41
hooddanielc <hood.danielc@gmail.com>
2015-05-07 21:31:58 -0700

    adding more todos for WaveFrontParser

--- front-end/app/mixins/dans-world/utilities/wave-front-parser.js
+++ front-end/app/mixins/dans-world/utilities/wave-front-parser.js
@@ -9,13 +9,33 @@ import Ember from 'ember';
 export default Ember.Object.extend({
   objFile: null,
   loadingPromise: null,
-  _httpLoaderPromises: null,
-  _rawQueue: null,
+  popMaxRaw: 10000,
+  popMaxLines: 5000,
+  idleRawTime: 1,
+  idleLineTime: 1,
+  _queuesProcessing: 0,
+  _rawQueue: "",
   _lineQueue: null,
-  _state: null,
-  _states: null,
-  _popMaxRaw: 4000,
-  _popMaxLines: 500,
+  _materialQueue: null,
+  _doneLoading: false,
+
+  _initInstanceProps: function () {
+    this.setProperties({
+      _rawQueue: [],
+      _lineQueue: [],
+      _materialQueue: []
+    });
+  }.on('init'),
+
+  _isDoneLoading: function () {
+    return (
+      this.get("_rawQueue").length === 0 &&
+      this.get("_lineQueue").length === 0 &&
+      this.get("_materialQueue").length === 0 &&
+      this.get("_queuesProcessing") === 0 &&
+      this.get("_doneLoading")
+    );
+  },
 
   /**
    * shifts an amount characters
@@ -23,17 +43,44 @@ export default Ember.Object.extend({
    * and turns them into lines
    */
   idleParseRawTick: function () {
-    // TODO
-  },
+    if (this._isDoneLoading()) {
+      return;
+    } else {
+      // TODO synchronous action here
+    }
+  }.on('init'),
 
   /**
    * Shifts an amount of lines
    * off of the line queue
-   * and invokes parse events
+   * and invokes parse events.
    */
-  idleParseLine: function () {
-    // TODO
-  },
+  idleParseLine: function idleParseLine() {
+    if (this._isDoneLoading()) {
+      return;
+    } else {
+      // TODO synchronous action here
+    }
+  }.on('init'),
+
+  /**
+   * Shifts an amount of materials
+   * off of the line queue and
+   * invokes parse events.
+   */
+  idleParseMaterial: function () {
+    if (this._isDoneLoading()) {
+      return;
+    } else {
+      // TODO synchronous action here
+    }
+  }.on('init'),
+
+  /**
+   * Shifts an amount of
+   * materials to start
+   * parsing through
+   */
 
   load: function () {
     var p = new Ember.RSVP.Promise(function () {
